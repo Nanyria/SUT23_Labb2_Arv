4d01d4ea836b857a357f3bb862bae233070254d5
Nathalee Wilund <nathalee.wilund@edu.varberg.se>
2023-10-26 13:43:21 +0200

    Skapat klasserna Djur, Katt, Hund och Drake. Lagt till egenskaper för Djur, en konstruktor för Djur samt tre virual methods för djur.

--- Djur.cs
+++ Djur.cs
@@ -11,15 +11,15 @@ namespace SUT23_Labb2_Arv
         public string _name;
         public int _age;
         public string _description;
-        public string _eyecolor;
+        public string _diet;
         public string _typeOfAnimal;
 
-        public Djur(string Name, int Age, string Description, string Eyecolor, string TypeOfAnimal)
+        public Djur(string Name, int Age, string Description, string Diet, string TypeOfAnimal)
         {
             _name = Name;
             _age = Age;
             _description = Description;
-            _eyecolor = Eyecolor;
+            _diet = Diet;
             _typeOfAnimal = TypeOfAnimal;
         }
 
@@ -28,6 +28,10 @@ namespace SUT23_Labb2_Arv
 
         }
         public virtual void AnimalInfo()
+        {
+
+        }
+        public virtual void Hunting()
         {
 
         }
