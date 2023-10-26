d2c8bb239d2f2cbd2a1edcd34e70e5b28bfb1e00
Nathalee Wilund <nathalee.wilund@edu.varberg.se>
2023-10-26 13:39:27 +0200

    Add project files.

---
+++ Djur.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SUT23_Labb2_Arv
+{
+    internal class Djur
+    {
+        public string _name;
+        public int _age;
+        public string _description;
+        public string _eyecolor;
+        public string _typeOfAnimal;
+
+        public Djur(string Name, int Age, string Description, string Eyecolor, string TypeOfAnimal)
+        {
+            _name = Name;
+            _age = Age;
+            _description = Description;
+            _eyecolor = Eyecolor;
+            _typeOfAnimal = TypeOfAnimal;
+        }
+
+        public virtual void makeSound()
+        {
+
+        }
+        public virtual void AnimalInfo()
+        {
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SUT23_Labb2_Arv
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
